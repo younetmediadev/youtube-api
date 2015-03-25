3aebb6b700a91edb4f2d87ca235d5a436a7ad4fb
Trần Hoàng Nhật <tranhoangnhat5683@gmail.com>
2015-03-25 08:55:07 +0700

    Update comment.js

--- api/v3.0.0/comment.js
+++ api/v3.0.0/comment.js
@@ -1,7 +1,38 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+/**
+ *  Copyright 2015 Just4Fun
+ *
+ *  A Node.JS module, which provides an object oriented wrapper for the Youtube v3 API.
+ *  Author: Just4Fun <tranhoangnhat5683@gmail.com>
+ *
+ * User profile by user id_social
+ * https://www.googleapis.com/plus/v1/people/<id_social>?key=<key>
+ * Reply of comment
+ * https://www.googleapis.com/plus/v1/activities/<id_social>/comments?key=<key>
+ **/
 
+var Util = require("../../util");
 
+function list (options, callback) {
+    var action = 'activities/' + options.id + '/comments';
+    var url = Util.createUrlV1.apply(this, [action, options]);
+    var reqOptions = {
+        url: url
+    };
+
+    this.Client.request(reqOptions, callback);
+}
+
+function get (options, callback) {
+    var action = 'activities/' + options.id;
+    var url = Util.createUrlV1.apply(this, [action, options]);
+    var reqOptions = {
+        url: url
+    };
+
+    this.Client.request(reqOptions, callback);
+}
+
+module.exports = {
+    list: list,
+    get: get
+};
